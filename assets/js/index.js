bb717d67f2ccf35c8d988814b114f1235e8200f8
TheoMNU <meneu.theo@gmail.com>
2023-11-25 18:16:13 +0100

    footer nav header ok

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,18 @@
-let burger = document.getElementById('burger')
 let navBar = document.getElementById('navBar')
+let btnNav = document.getElementById('btnNav')
+let burger = document.getElementById('burger')
+let cross = document.getElementById('cross')
 
 burger.addEventListener('click', () => {
     navBar.classList.toggle('active-navBar')
+    navBar.classList.remove('desactive-navBar')
+    burger.classList.toggle('desactive')
+    cross.classList.toggle('desactive')
+})
+
+cross.addEventListener('click', () => {
+    burger.classList.toggle('desactive')
+    cross.classList.toggle('desactive')
+    navBar.classList.remove('active-navBar')
+    navBar.classList.add('desactive-navBar')
 })

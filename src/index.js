37ba056da079bd7ad86b385599cad3c094f9f21e
TheoMNU <meneu.theo@gmail.com>
2023-12-10 12:05:31 +0100

    Men in Blate V1.0

--- src/index.js
+++ src/index.js
@@ -3,16 +3,34 @@ let btnNav = document.getElementById('btnNav')
 let burger = document.getElementById('burger')
 let cross = document.getElementById('cross')
 
-burger.addEventListener('click', () => {
-    navBar.classList.toggle('active-navBar')
-    navBar.classList.remove('desactive-navBar')
+function burgerAction() {
     burger.classList.toggle('desactive')
     cross.classList.toggle('desactive')
+}
+
+burger.addEventListener('click', () => {
+    burgerAction()
+    navBar.classList.toggle('active-navBar')
+    navBar.classList.remove('desactive-navBar')
 })
 
 cross.addEventListener('click', () => {
-    burger.classList.toggle('desactive')
-    cross.classList.toggle('desactive')
+    burgerAction()
     navBar.classList.remove('active-navBar')
     navBar.classList.add('desactive-navBar')
+})
+
+//----------------- Button Top ---------------- /
+let btnTop = document.querySelector('.go-head')
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > window.innerHeight) {
+        btnTop.classList.add('active')
+    } else {
+        btnTop.classList.remove('active')
+    }
+})
+
+btnTop.addEventListener('click', () => {
+    btnTop.classList.remove('active')
 })
